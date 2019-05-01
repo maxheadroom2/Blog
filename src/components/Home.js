2137a591c5aae938f1dc76b3ba3100c817a3ec08
Emiliano Garza <emiliano.jgj97@gmail.com>
2019-04-30 21:54:44 -0500

    Terminado trabajo del día martes 30 de abril

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,8 +12,18 @@ const Home = () => {
                     <Row>
                       <Col sm={3}>
                         <ListGroup className='w-sh'>
-                        <ListGroup.Item variant="info">
-                          Entries
+                        <ListGroup.Item>
+                          <Card className="bg-dark text-white">
+                            <Card.Img src="holder.js/100px270" alt="Card image" />
+                            <Card.ImgOverlay>
+                              <Card.Title>Card title</Card.Title>
+                              <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                              </Card.Text>
+                              <Card.Text>Last updated 3 mins ago</Card.Text>
+                            </Card.ImgOverlay>
+                          </Card>
                         </ListGroup.Item>
                           <ListGroup.Item action href="#link1">
                             Link 1
